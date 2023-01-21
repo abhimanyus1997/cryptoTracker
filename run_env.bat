b869c733c0199112d55f4d4e6c6282b46998845f
Abhimanyu Singh <abhimanyus1997@gmail.com>
2023-01-21 18:37:58 +0530

    Updated Run script for windows

--- run_env.bat
+++ run_env.bat
@@ -1,13 +1,25 @@
 @ECHO OFF
 
-ECHO "Starting virtual environment"
+ECHO Starting virtual environment
+ECHO ------------------------------
 
-ECHO "Deactivating old environments"
+ECHO Deactivating old environments
+ECHO ------------------------------
 python virtualenv deactivate
 
 ECHO "Opening Scripts"
+ECHO "------------------------------"
 CD "env\Scripts"
 
 ECHO "Executing venv using Powershell"
+ECHO "------------------------------"
 powershell.exe -command "activate.ps1"
+ECHO "                              "
+
+ECHO "Starting Flask Server"
+ECHO "------------------------------"
+cd ..\..\Flask
+flask --app server run --host=0.0.0.0
+
+
 echo "The program has completed"
