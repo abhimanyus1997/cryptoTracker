efb1902f47f900a2750044fa2bf7f887d6373a64
Abhimanyu Singh <abhimanyus1997@gmail.com>
2023-01-23 03:08:49 +0530

    Migrated to Django Server

---
+++ run_django.bat
@@ -0,0 +1,25 @@
+@ECHO OFF
+
+ECHO Starting virtual environment
+ECHO ------------------------------
+
+ECHO Deactivating old environments
+ECHO ------------------------------
+python virtualenv deactivate
+
+ECHO "Opening Scripts"
+ECHO "------------------------------"
+CD "env\Scripts"
+
+ECHO "Executing venv using Powershell"
+ECHO "------------------------------"
+powershell.exe -command "activate.ps1"
+ECHO "                              "
+
+ECHO "Starting Django Server"
+ECHO "------------------------------"
+cd ..\..\django
+py manage.py runserver
+
+
+echo "The program has completed"
