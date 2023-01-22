3e1a23b96484c91dca78f421f0643a098a93d012
Abhimanyu Singh <abhimanyus1997@gmail.com>
2023-01-23 02:49:27 +0530

    Optimized the code for Flask

--- flask_v2/server.py
+++ flask_v2/server.py
@@ -11,15 +11,29 @@ def index():
     wallet = "0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045" #vitalik's address
     return render_template('index.html', title='Welcome', username=name, erc20=wallet)
 
-@app.route('/hi')
-def hello_world():
-   return 'hello world'
+
+@app.route('/login/')
+def login():
+    return render_template('login.html')
 
 
-@app.route('/hello/<name>')
-def hello_name(name):
-    x = "Hello "+str(name)
-    return x
+@app.route('/profile/')
+def profile():
+    return render_template('profile.html')
+
+
+@app.route('/register/')
+def register():
+    return render_template('register.html')
+
+
+@app.route('/table/')
+def table():
+    return render_template('table.html')
+
+@app.errorhandler(404)
+def not_found(e):
+    return render_template('404.html')
 
 if __name__ =='__main__':
     app.run(host='0.0.0.0', port=80,debug = True)
