d78f2a7c2049bc7affbb8a94c20c2783664f55c4
Abhimanyu Singh <abhimanyus1997@gmail.com>
2023-01-29 20:49:29 +0530

    Added plotly and matplotlib

--- run_Flaskenv.bat
+++ run_Flaskenv.bat
@@ -6,17 +6,21 @@ ECHO ------------------------------
 @REM ECHO Deactivating old environments
 TITLE "cryptoTracker Flask Server - abhimanyus1997"
 
-COLOR 1
+
 ECHO ----------Opening Scripts----------
 CD "env\Scripts"
 
-COLOR 2
+
 ECHO ----------Executing venv using CMD----------
 cmd.exe /c activate.bat
 
+
+@REM ECHO ----------Exporting environment----------
+@REM python /c pip install pip-chill
+@REM python pip-chill --no-version > requirements.txt
+
 COLOR 5
 ECHO ----------Starting Flask Server----------
-cd ..\..\Flask
 python main.py
 
 COLOR
