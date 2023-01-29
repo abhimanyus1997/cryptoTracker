7507dad32cc492e14c012a427b2fa32ef6a6f0e1
Abhimanyu Singh <abhimanyus1997@gmail.com>
2023-01-29 23:59:39 +0530

    Added Login & Registration with personal page

--- flask_app/models.py
+++ flask_app/models.py
@@ -17,4 +17,5 @@ class User(db.Model, UserMixin):
     password = db.Column(db.String(150))
     first_name = db.Column(db.String(150))
     last_name = db.Column(db.String(150))
-    erc_addresses = db.relationship('ERCaddress')
+    erc20 = db.Column(db.String(50))
+    #erc_addresses = db.relationship('ERCaddress')
