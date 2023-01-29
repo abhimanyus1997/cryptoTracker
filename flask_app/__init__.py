9a787f2ddf2c37ca527925e8279449ebdb54141b
Abhimanyu Singh <abhimanyus1997@gmail.com>
2023-01-29 22:20:38 +0530

    Added Database using SQLAlchemy

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -1,15 +1,18 @@
 from flask import Flask, render_template, redirect
 from markupsafe import escape
 from flask_sqlalchemy import SQLAlchemy
+from os import path
 
+# create the extension
 db = SQLAlchemy()
 DB_NAME = "database.db"
 
 def create_app():
+    # create the app
     app = Flask(__name__)
     app.config['SECRET_KEY'] = "VWHSBJJBHWJBJSBjbjhdbhjbdhvbh"
+    # configure the SQLite database, relative to the app instance folder
     app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{DB_NAME}"
-
     # initializing DB
     db.init_app(app)
 
@@ -18,5 +21,17 @@ def create_app():
 
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
+    # Check database
+    from .models import User, ERCaddress
+    create_database(app)
 
     return app
+
+# To create database if doesn't exist
+def create_database(app):
+    if not path.exists("flask_app"+DB_NAME):
+        # requires an application context
+        with app.app_context():
+            # to create the table schema
+            db.create_all()
+        print("Created Database!")
