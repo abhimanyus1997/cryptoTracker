3e6b38f0f4d51063b90f9fd7cc12ea692b74a10f
Abhimanyu Singh <abhimanyus1997@gmail.com>
2023-01-29 21:43:48 +0530

    Updated Etherscanner SCV

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -10,6 +10,9 @@ def create_app():
     app.config['SECRET_KEY'] = "VWHSBJJBHWJBJSBjbjhdbhjbdhvbh"
     app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{DB_NAME}"
 
+    # initializing DB
+    db.init_app(app)
+
     from .views import views
     from .auth import auth
 
