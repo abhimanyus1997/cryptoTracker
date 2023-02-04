5077e8e975066b75ce9d3041051bbb8e83efdf53
Abhimanyu Singh <abhimanyus1997@gmail.com>
2023-02-04 10:33:41 +0530

    updated code

--- flask_app/cryptotracker_api.py
+++ flask_app/cryptotracker_api.py
@@ -5,7 +5,7 @@ import pandas as pd
 base_url = "https://api.ethplorer.io"
 
 
-def getTokensCSV(address, filename="export_csv"):
+def getTokensCSV(address, export_csv=False, filename="export_csv"):
     """
     Returns no of tokens for an address and saves a CSV
     """
@@ -25,18 +25,19 @@ def getTokensCSV(address, filename="export_csv"):
     no_of_tokens = len(token_list)
 
     #code to generate CSV
-    flag = True
-    for token in token_list:
-        token_info = pd.DataFrame([token["tokenInfo"]])
-        token_ = token.copy()
-        token_.pop("tokenInfo")
-        balance_info = pd.DataFrame([token_])
-        info = pd.concat([token_info, balance_info], axis=1)
-        # Run once
-        if flag:
-            columns = list(info.columns)
-            df = pd.DataFrame(columns=columns)
-            flag = False
-        df = pd.concat([df, info], axis=0)
-        df.to_csv(f"{filename}.csv")
-        return no_of_tokens
+    if export_csv:
+        flag = True
+        for token in token_list:
+            token_info = pd.DataFrame([token["tokenInfo"]])
+            token_ = token.copy()
+            token_.pop("tokenInfo")
+            balance_info = pd.DataFrame([token_])
+            info = pd.concat([token_info, balance_info], axis=1)
+            # Run once
+            if flag:
+                columns = list(info.columns)
+                df = pd.DataFrame(columns=columns)
+                flag = False
+            df = pd.concat([df, info], axis=0)
+            df.to_csv(f"{filename}.csv")
+    return no_of_tokens
