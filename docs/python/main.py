454865e0540581a707d48ad48ebe17df65ed9683
Abhimanyu Singh <abhimanyus1997@gmail.com>
2023-12-19 00:32:05 +0530

    Added Starter Plotting and Pandas Support

--- docs/python/main.py
+++ docs/python/main.py
@@ -1,7 +1,49 @@
-from pyscript import window, document
+import pandas as pd
+from pyweb import pydom
+from pyodide.http import open_url
+from pyscript import display, when, document, window
+from js import console
+import matplotlib.pyplot as plt
 
 
-my_element = document.querySelector("#myip")
-my_element.innerText = "Your IP: "+window.location.hostname
+def log(message):
+    # log to pandas dev console
+    print(message)
+    # log to JS console
+    console.log(message)
+
+
+@when("click", "#csvBtn")
+def click_handler(event):
+    """
+    Event handlers get an event object representing the activity that raised
+    them.
+    """
+    # pydom["div#pandas-output-inner"].html = ""
+    url = document.querySelector("#csvFile").value
+
+    # log(f"Trying to fetch CSV from {url}")
+    df = pd.read_csv(open_url(url))
+    df.plot()
+    # plt.gcf().canvas.set_window_title('Pandas Plot')
+    # pydom["div#pandas-output"].style["display"] = "block"
+    # pydom["div#pandas-dev-console"].style["display"] = "block"
+    # document.querySelector("#test").innerText = url
+    # pydom["div#test"].style["display"] = "block"
+
+    # Create a string representation of the describe() output
+    description = df.describe().to_html()
+    document.querySelector("#test").style.display = "block"
+    # document.querySelector("#test").innerText = description
+    # display(description, target="test", append="True")
+    display(df.describe(), target="test", append="True")
+    df.plot()
+    plt.show()
+
+
+
+
+
+
 
 
