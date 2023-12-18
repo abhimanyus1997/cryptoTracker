578a5e97bf6dc3bff5c4985f4b92474fbf28afc3
Abhimanyu Singh <abhimanyus1997@gmail.com>
2023-12-18 21:18:17 +0530

    Added Version info

--- docs/python/main.py
+++ docs/python/main.py
@@ -1,9 +1,5 @@
-import arrr
-from pyscript import document
+# from pyscript import window, document
 
 
-def translate_english(event):
-    input_text = document.querySelector("#english")
-    english = input_text.value
-    output_div = document.querySelector("#output")
-    output_div.innerText = arrr.translate(english)
+# my_element = document.querySelector("#tableContainer")
+# my_element.innerText = window.location.hostname
