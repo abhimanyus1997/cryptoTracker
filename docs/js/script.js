fe6b3de3348712c70346de85a41848e3c57e1289
Abhimanyu Singh <abhimanyus1997@gmail.com>
2023-12-18 18:54:30 +0530

    Added Pyscript Support

--- docs/js/script.js
+++ docs/js/script.js
@@ -10,3 +10,43 @@ function takeScreenshot() {
         downloadLink.click();
     });
 }
+
+
+// CSV reader not working now
+// document.addEventListener("DOMContentLoaded", function () {
+//     const myForm = document.getElementById("myForm");
+//     const tableContainer = document.getElementById("tableContainer");
+
+//     myForm.addEventListener("submit", function (e) {
+//         e.preventDefault();
+//         const csvFile = document.getElementById("csvFile");
+//         const input = csvFile.files[0];
+
+//         if (input) {
+//             const reader = new FileReader();
+//             reader.onload = function (e) {
+//                 const text = e.target.result;
+//                 const rows = text.split("\n");
+//                 const table = document.createElement("table");
+//                 table.classList.add("table", "table-striped");
+
+//                 rows.forEach(row => {
+//                     const rowData = row.split(",");
+//                     const tr = document.createElement("tr");
+//                     rowData.forEach(cellData => {
+//                         const td = document.createElement("td");
+//                         td.textContent = cellData;
+//                         tr.appendChild(td);
+//                     });
+//                     table.appendChild(tr);
+//                 });
+
+//                 tableContainer.innerHTML = ""; // Clear previous content
+//                 tableContainer.appendChild(table);
+//             };
+//             reader.readAsText(input);
+//         } else {
+//             console.error("No file selected.");
+//         }
+//     });
+// });
